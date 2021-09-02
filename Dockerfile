522a64429c00733366ab3f0bdd6bcb2cc7f2162c
Roberto García <roberto@rhizomik.net>
2021-09-02 11:12:00 +0200

    Attempt with iExec v6 and Viviani

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,7 @@
-FROM registry.scontain.com:5050/sconecuratedimages/apps:node-10.14-alpine
+FROM node:14-alpine3.11
 
-RUN apk add bash nodejs-npm
-RUN SCONE_MODE=sim npm install axios@0.21.x ethers@5.0.x
+RUN mkdir /app && cd /app && npm install axios@0.21.x ethers@5.0.x
 
 COPY ./src/app /app
-
-COPY ./protect-fs.sh ./Dockerfile /build/
-RUN sh /build/protect-fs.sh /app
 
 ENTRYPOINT [ "node", "/app/index.js"]
--- src/app/index.js
+++ src/app/index.js
@@ -18,6 +18,7 @@ async function getAPIKey(API_KEY_PATH) {
     try {
         const iexecIn = process.env.IEXEC_IN || 'iexec_in';
         const iexecOut = process.env.IEXEC_OUT || 'iexec_out';
+        const datasetFileName = process.env.IEXEC_DATASET_FILENAME || 'key.txt';
 
         if (process.argv.length < 4) {
             throw new Error('Parameters for videoId and content hash are required');
@@ -25,7 +26,7 @@ async function getAPIKey(API_KEY_PATH) {
         const id = process.argv[2];
         const hash = process.argv[3];
 
-        const API_KEY = await getAPIKey(`${iexecIn}/key.txt`);
+        const API_KEY = await getAPIKey(`${iexecIn}/${datasetFileName}`);
         const args = { part: 'snippet', id: id, key: API_KEY };
         let result = await axios.get(API_URL, { params: args });
 
